42cb1f7826644e7325f692c16af06f7e0888999b
= <=>
2023-10-27 08:48:05 +0200

    test

--- src/assets/playerstore.js
+++ src/assets/playerstore.js
@@ -1,14 +1,12 @@
 import Projectile from './Projectile.js'
-import spriteImage from './assets/sprites/Idle Run (78x58).png'
-import Sound from './Sound.js'
 
 export default class Player {
   constructor(game) {
     this.game = game
-    this.width = 78
-    this.height = 58
-    this.x = 50
-    this.y = 100
+    this.width = 32
+    this.height = 64
+    this.x = this.game.width / 2 - this.width / 2
+    this.y = this.game.height / 2 - this.height / 2
 
     this.projectiles = []
 
@@ -22,25 +20,6 @@ export default class Player {
     this.ammoInterval = 500
 
     this.lives = 10
-
-    // adding sprite image
-    const image = new Image()
-    image.src = spriteImage
-    this.image = image
-
-    // sprite animation
-    this.frameX = 0
-    this.frameY = 1
-    this.maxFrame = 8
-    this.fps = 20
-    this.timer = 0
-    this.interval = 1000 / this.fps
-
-    // flip sprite direction
-    this.flip = false
-
-    // sound
-    this.sound = new Sound(this.game)
   }
 
   update(deltaTime) {
@@ -87,26 +66,6 @@ export default class Player {
     this.projectiles = this.projectiles.filter(
       (projectile) => !projectile.markedForDeletion
     )
-  
-    // flip sprite direction
-    if (this.speedX < 0) {
-      this.flip = true
-    } else if (this.speedX > 0) {
-      this.flip = false
-    }
-
-    // sprite animation update
-    if (this.timer > this.interval) {
-      this.frameX++
-      this.timer = 0
-    } else {
-      this.timer += deltaTime
-    }
-
-    // reset frameX when it reaches maxFrame
-    if (this.frameX >= this.maxFrame) {
-      this.frameX = 0
-    }
   }
 
   draw(context) {
@@ -128,25 +87,9 @@ export default class Player {
       context.stroke()
     }
 
-    // draw sprite image
-    if (this.flip) {
-      context.save()
-      context.scale(-1, 1)
-    }
-
-    context.drawImage(
-      this.image,
-      this.frameX * this.width,
-      this.frameY * this.height - 14,
-      this.width,
-      this.height,
-      this.flip ? this.x * -1 - this.width : this.x,
-      this.y,
-      this.width,
-      this.height
-    )
-
-    context.restore()
+    this.projectiles.forEach((projectile) => {
+      projectile.draw(context)
+    })
   }
 
   shoot(mouseX, mouseY) {
