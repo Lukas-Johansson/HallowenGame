417e76ef5eafb507b8a61806485048401bd67775
Lukas <lukas.johansson090@gmail.com>
2023-11-03 14:56:10 +0100

    test

--- src/Pumpkin.js
+++ src/Pumpkin.js
@@ -1,24 +1,24 @@
-import Enemy from './Enemy.js'
+import Enemy from './Enemy.js';
+const pumpkin = new Pumpkin(gameInstance, x, y, ''); // Replace 'path/to/pumpkin.png' with the actual path to your pumpkin image
 
 export default class Pumpkin extends Enemy {
-  constructor(game, x, y) {
-    super(game)
-    this.width = 32
-    this.height = 32
-    this.x = x
-    this.y = y
-    this.speed = 1
-    this.lives = Math.floor(Math.random() * 3) + 1
-    this.color = 'white'
+  constructor(game, x, y, imageSrc) {
+    super(game, imageSrc); // Pass the image source here
+    this.width = 32;
+    this.height = 32;
+    this.x = x;
+    this.y = y;
+    this.speed = 1;
+    this.lives = Math.floor(Math.random() * 3) + 1;
   }
 
   update(player) {
-    const dx = player.x - this.x // calculate the x distance to the player
-    const dy = player.y - this.y // calculate the y distance to the player
-    const distance = Math.sqrt(dx * dx + dy * dy) // calculate the total distance to the player
-    const speedX = (dx / distance) * this.speed // calculate the x speed towards the player
-    const speedY = (dy / distance) * this.speed // calculate the y speed towards the player
-    this.x += speedX // move the enemy towards the player on the x axis
-    this.y += speedY // move the enemy towards the player on the y axis
+    const dx = player.x - this.x;
+    const dy = player.y - this.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    const speedX = (dx / distance) * this.speed;
+    const speedY = (dy / distance) * this.speed;
+    this.x += speedX;
+    this.y += speedY;
   }
 }
