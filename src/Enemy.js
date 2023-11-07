e41faa40089a167a1f5d6a90f8e93f32a3d69371
= <=>
2023-11-07 14:40:58 +0100

    galen sprites

--- src/Enemy.js
+++ src/Enemy.js
@@ -1,34 +1,56 @@
 export default class Enemy {
-  constructor(game, imageUrl) {
-    this.game = game;
-    this.x = 0;
-    this.y = 0;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.markedForDeletion = false;
-    this.image = new Image();
-    this.image.onload = () => {
-      // You can set the width and height here if needed
-    };
-    this.image.src = imageUrl;
-    this.type = 'enemy';
+  constructor(game, color) {
+    this.game = game
+    this.x = 0
+    this.y = 0
+    this.width = 0
+    this.height = 0
+    this.speedX = 0
+    this.speedY = 0
+    this.markedForDeletion = false
+    this.color = color
+    this.type = 'enemy'
   }
 
   update() {
-    this.y += this.speedY;
-    this.x += this.speedX;
-    if (this.x < 0 || this.x > this.game.width) this.markedForDeletion = true;
-    if (this.y < 0 || this.y > this.game.height) this.markedForDeletion = true;
+    this.y += this.speedY
+    this.x += this.speedX
+    if (this.x < 0 || this.x > this.game.width) this.markedForDeletion = true
+    if (this.y < 0 || this.y > this.game.height) this.markedForDeletion = true
+
   }
 
   draw(context) {
-    // Only draw the image if it's loaded
-    if (this.image.complete) {
-      context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    context.fillStyle = this.color
+    context.fillRect(this.x, this.y, this.width, this.height)
 
-      if (this.game.debug) {
-        // Add your debug code here...
-      }
+    if (this.game.debug) {
+      context.strokeRect(this.x, this.y, this.width, this.height)
+      context.fillStyle = 'black'
+      context.font = '20px Arial'
+      context.fillText(this.lives, this.x, this.y - 5)
+      context.font = '12px Arial'
+      context.fillText(`x: ${this.x.toFixed()}`, this.x + 20, this.y - 5)
+      context.fillText(`y: ${this.y.toFixed()}`, this.x + 20, this.y - 20)
     }
+
+    if (this.flip) {
+      context.save()
+      context.scale(-1, 1)
+    }
+
+    context.drawImage(
+      this.image,
+      this.frameX * this.width,
+      0,
+      this.width,
+      this.height,
+      this.flip ? this.x * -1 - this.width : this.x,
+      this.y,
+      this.width,
+      this.height
+    )
+
+    context.restore()
   }
 }
