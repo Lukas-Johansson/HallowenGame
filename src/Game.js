c72c75c63d26e0a09fc341ab1191c499b9716e69
Lukas <lukas.johansson090@gmail.com>
2023-11-08 17:50:52 +0100

    Spöken kan inte klumpas ihop

--- src/Game.js
+++ src/Game.js
@@ -25,7 +25,7 @@ export default class Game {
         this.wave = 0; // Current wave
         this.enemyTimer = 0;
         this.enemyInterval = 1; // Adjust this interval as needed (e.g., increase it)
-        this.enemiesPerWave = 1; // Number of enemies per wave (e.g., reduce it)    
+        this.enemiesPerWave = 1; // Number of enemies per wave (e.g., reduce it)
         this.enemiesSpawnedInWave = 0; // Number of enemies spawned in the current wave
         this.player = new Player(this);
         this.waveInProgress = true; // Initialize the flag to indicate that a wave is in progress
@@ -35,7 +35,7 @@ export default class Game {
     update(deltaTime) {
         if (!this.startGame) {
             this.player.ammo = 0;
-            return
+            return;
         }
 
         if (this.wave === 0) {
@@ -43,18 +43,17 @@ export default class Game {
         }
 
         if (this.gameReset) {
-            this.reset()
-            this.player = new Player(this)
-            this.gameReset = false
+            this.reset();
+            this.player = new Player(this);
+            this.gameReset = false;
         }
 
         if (!this.gameOver) {
-            this.gameTime += deltaTime
+            this.gameTime += deltaTime;
         }
 
-        //stop game
         if (this.gameOver) {
-            return
+            return;
         }
 
         this.player.update(deltaTime);
@@ -79,15 +78,15 @@ export default class Game {
 
                 for (let i = 0; i < enemiesToSpawn; i++) {
                     const margin = 15; // Adjust this margin value as needed
-                
+
                     // Calculate the range within which candies should spawn
                     const xRange = this.width - 2 * margin;
                     const yRange = this.height - 2 * margin;
-                
+
                     // Generate random positions within the range
                     let x = margin + Math.random() * xRange;
                     let y = margin + Math.random() * yRange;
-                
+
                     if (Math.random() < 0.2) {
                         this.usable.push(new Candy(this, x + 75, y + 75));
                     } else {
@@ -112,6 +111,7 @@ export default class Game {
 
         this.player.update(deltaTime);
 
+        // Collision detection and resolution for pumpkins
         this.enemies.forEach((enemy) => {
             enemy.update(deltaTime, this.player);
             if (this.checkCollision(this.player, enemy)) {
@@ -119,8 +119,15 @@ export default class Game {
                 enemy.markedForDeletion = true;
             }
 
+            // Check collisions with other pumpkins and resolve them
+            this.enemies.forEach((otherEnemy) => {
+                if (enemy !== otherEnemy && this.checkCollision(enemy, otherEnemy)) {
+                    this.resolveCollision(enemy, otherEnemy);
+                }
+            });
+
             this.player.projectiles.forEach((projectile) => {
-                if (this.checkCollision(projectile, enemy,)) {
+                if (this.checkCollision(projectile, enemy)) {
                     if (enemy.lives > 1) {
                         enemy.lives -= projectile.damage;
                     } else {
@@ -130,6 +137,8 @@ export default class Game {
                 }
             });
         });
+
+        // Filter out deleted enemies
         this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion);
 
         this.usable.forEach((usable) => {
@@ -140,7 +149,7 @@ export default class Game {
             }
 
             this.player.projectiles.forEach((projectile) => {
-                if (this.checkCollision(projectile, usable,)) {
+                if (this.checkCollision(projectile, usable)) {
                     if (usable.lives > 1) {
                         usable.lives -= projectile.damage;
                     } else {
@@ -151,11 +160,15 @@ export default class Game {
                 }
             });
         });
+
+        // Filter out deleted usable items
         this.usable = this.usable.filter((usable) => !usable.markedForDeletion);
 
         if (this.player.lives <= 0) {
+            // Handle game over
         }
     }
+
 
     reset() {
         this.keys = [];
@@ -214,6 +227,23 @@ export default class Game {
             object1.y < object2.y + object2.height &&
             object1.y + object1.height > object2.y
         );
+    }
+
+    resolveCollision(pumpkin1, pumpkin2) {
+        const dx = pumpkin2.x - pumpkin1.x;
+        const dy = pumpkin2.y - pumpkin1.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        const overlap = (pumpkin1.width + pumpkin2.width) / 2 - distance;
+
+        if (overlap > 0) {
+            // Move the pumpkins away from each other
+            const moveX = (dx / distance) * overlap;
+            const moveY = (dy / distance) * overlap;
+            pumpkin1.x -= moveX;
+            pumpkin1.y -= moveY;
+            pumpkin2.x += moveX;
+            pumpkin2.y += moveY;
+        }
     }
     
 }
